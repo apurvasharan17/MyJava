224673ade816438a3824437ec2ecfd950ffd9dcf
Apurva Sharan <ranjan.apurva2017@gmail.com>
2024-03-12 23:24:05 +0530

    added files

--- BinarySearchTree/BST.java
+++ BinarySearchTree/BST.java
@@ -34,6 +34,18 @@ public class BST {
         System.out.print(root.data+" ");
         inorder(root.right);
     }
+    public static boolean search(Node root,int key){
+        if(root==null){
+            return false;
+        }
+        if(root.data==key){
+                return true;
+        }else if(root.data>key){
+            return search(root.left,key);
+        }else{
+         return search(root.right,key);
+         }
+    }
     public static void main(String[] args) {
         int values[]={5,1,3,4,2,7};
         Node root=null;
@@ -42,5 +54,12 @@ public class BST {
             root=insert(root,values[i]);
         }
         inorder(root);
+
+        if(search(root,7)){
+            System.out.println("found");
+        }
+        else{
+            System.out.println("not found");
+        }
     }
 }
