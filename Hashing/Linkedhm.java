4ace14da8e930811fd6c9d2bea09f7dc7bd4a087
Apurva Sharan <ranjan.apurva2017@gmail.com>
2024-03-15 22:59:51 +0530

    Hashing

---
+++ Hashing/Linkedhm.java
@@ -0,0 +1,25 @@
+package Hashing;
+import java.util.*;
+public class Linkedhm {
+    
+
+    public static void main(String[] args) {
+        LinkedHashMap<String,Integer> lhm=new LinkedHashMap<>();
+        lhm.put("India",100);
+        lhm.put("China",150);
+        lhm.put("US",50);
+
+        HashMap<String,Integer> hm=new HashMap<>();
+        hm.put("India",100);
+        hm.put("China",150);
+        hm.put("US",50);
+
+        System.out.println(lhm);
+        System.out.println(hm);
+
+
+
+
+    }
+
+}
